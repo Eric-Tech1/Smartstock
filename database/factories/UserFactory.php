4d1752409421c4d1e605ea253e275368fbf6d28a
Ben Eric <dudezwrld@gmail.com>
2026-05-16 00:08:16 +0100

    Fix: Instantiate Faker factory generator manually

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,17 +22,19 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
-        return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
-        ];
-    }
+   public function definition(): array
+{
+    // Manually instantiate the Faker container property to guarantee it's not null
+    $faker = \Faker\Factory::create();
 
+    return [
+        'name' => $faker->name(),
+        'email' => $faker->unique()->safeEmail(),
+        'email_verified_at' => now(),
+        'password' => static::$password ??= Hash::make('password'),
+        'remember_token' => Str::random(10),
+    ];
+}
     /**
      * Indicate that the model's email address should be unverified.
      */
